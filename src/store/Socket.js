5c8efd7b8ab270fd2eec410987764f02151577df
k.santhosh <santh2000916@gmail.com>
2021-05-04 11:19:59 +0530

    update

--- src/store/Socket.js
+++ src/store/Socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 import { AddNotification } from "./action";
-const url  = "http://localhost:3001";
+const url  = "https://stjbookbankbackend.herokuapp.com";
+// const url = "http://localhost:3001";
 
 
 var Socket;
